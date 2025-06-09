96d9d58149445d6e447e8749e4efbe5df920f838
FazrrProgrammers <ibung512@gmail.com>
2025-06-09 10:45:16 +0700

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+    datasets: [{
+      label: 'Visitors',
+      data: [12, 19, 3, 5, 2],
+      backgroundColor: 'rgba(54, 162, 235, 0.7)',
+    }]
+  },
+  options: {
+    responsive: true,
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
